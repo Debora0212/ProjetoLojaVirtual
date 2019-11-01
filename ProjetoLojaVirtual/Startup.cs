ffcb3d7866a1ab4725b3e3a4d129e314b5063567
Debora0212 <debora_s_brito@hotmail.com>
2019-11-01 18:13:34 -0300

    Criação do projeto Loja Virtual com template configurado

--- ProjetoLojaVirtual/Startup.cs
+++ ProjetoLojaVirtual/Startup.cs
@@ -49,15 +49,17 @@ namespace ProjetoLojaVirtual
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            /*
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            */
         }
     }
 }
