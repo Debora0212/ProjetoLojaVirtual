24974a768059cfa6228600744269d3711eaa5142
Debora0212 <debora_s_brito@hotmail.com>
2019-11-05 18:25:00 -0300

    Formulario contato, recebendo os dados, config o envio de email, msg de sucesso, erro, validacao, manter campo preenchido na validacao.

--- ProjetoLojaVirtual/Controllers/HomeController.cs
+++ ProjetoLojaVirtual/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoLojaVirtual.Libraries.Email;
+using ProjetoLojaVirtual.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace ProjetoLojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        private object stringBuilder;
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +24,46 @@ namespace ProjetoLojaVirtual.Controllers
             return View();
         }
 
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var listaMensagem = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagem, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach(var texto in listaMensagem)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+               
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente mais tarde!";
+
+                //TODO - Implentar log
+            }
+            
+            return View("Contato");
+        }
         public IActionResult Login()
         {
             return View();
---
+++ ProjetoLojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,46 @@
+﻿using ProjetoLojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ProjetoLojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("deb.santos.0212@gmail.com", "");
+                smtp.EnableSsl = true;
+            }
+
+            string corpoMsg = string.Format("<h2>Contato - Loja Virtual</h2>" +
+                "<b>Nome: </b> {0} <br />" +
+                "<b>Email: </b> {1} <br />" +
+                "<b>Texto: </b> {2} <br />" +
+                "<br />Email enviado automaticamente do site Loja Virtual.",
+                contato.Nome,
+                contato.Email,
+                contato.Texto
+                );
+
+            /*
+             * MailMessage -> construir a mensagem
+             * */
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("deb.santos.0212@gmail.com");
+            mensagem.To.Add("deb.santos.0212@gmail.com");
+            mensagem.Subject = "Contato - LojaVirtual - Email:" + contato.Email;
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            //Enviar mensagem via SMTP
+            smtp.Send(mensagem);
+        }
+    }
+}
