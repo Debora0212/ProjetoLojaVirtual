d11e2268ed24fa2e5db1e605e7b5cc82103100b0
Debora0212 <debora_s_brito@hotmail.com>
2019-11-12 16:58:04 -0300

    Area Colaborador - Classe Categoria.

---
+++ ProjetoLojaVirtual/Models/Categoria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLojaVirtual.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        /*
+         * Nome: Telefone sem fio
+         * Slug: telefone-sem-fio
+         */
+        public int Slug { get; set; }
+
+
+        /*
+         * Auto-relacionamento
+         * 1-Informatica - P=null
+         * - 2-Mouse - P=1
+         * -- 3-Mouse sem fio - P=2
+         * -- 4-Mouse gamer - P=2
+         */
+        public int? CategoriaPaiId { get; set; }
+
+         /*
+         * ORM - EntityFrameworkCore
+         */
+         [ForeignKey("CategoriaPaiId")]
+        public virtual Categoria CategoriaPai { get; set; }
+    }
+}
