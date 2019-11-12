1d4e15cc9dbf93700b7656cd2169035500b8d38f
Debora0212 <debora_s_brito@hotmail.com>
2019-11-12 19:25:43 -0300

    Area Colaborador - Categoria Index.

---
+++ ProjetoLojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLojaVirtual.Libraries.Filtro;
+using ProjetoLojaVirtual.Models;
+using ProjetoLojaVirtual.Repositories.Contracts;
+
+namespace ProjetoLojaVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    //TODO-Habilitar verificacao de login
+    //[ColaboradorAutorizacao]
+    public class CategoriaController : Controller
+    {
+        private ICategoriaRepository _categoriaRepository;
+
+        public CategoriaController(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult Index()
+        {
+           List<Categoria> categorias = _categoriaRepository.ObterTodasCategorias().ToList();
+            return View(categorias);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm]Categoria categoria)
+        {
+            //TODO-Implementar
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Atualizar([FromForm] Categoria categoria)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int Id)
+        {
+            return View();
+        }
+
+    }
+}
