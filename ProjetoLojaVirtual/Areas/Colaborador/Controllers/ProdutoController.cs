5a59281c95c10f4ea28b3cdf3932fe5fb79ab237
Debora0212 <debora_s_brito@hotmail.com>
2019-11-21 18:11:38 -0300

    Area Colaborador - Produto Cadastrar -Part 1 e 2.

--- ProjetoLojaVirtual/Areas/Colaborador/Controllers/ProdutoController.cs
+++ ProjetoLojaVirtual/Areas/Colaborador/Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjetoLojaVirtual.Repositories.Contracts;
 
 namespace ProjetoLojaVirtual.Areas.Colaborador.Controllers
@@ -11,14 +12,24 @@ namespace ProjetoLojaVirtual.Areas.Colaborador.Controllers
     public class ProdutoController : Controller
     {
         private IProdutoRepository _produtoRepository;
-        public ProdutoController(IProdutoRepository produtoRepository)
+        private ICategoriaRepository _categoriaRepository;
+
+        public ProdutoController(IProdutoRepository produtoRepository, ICategoriaRepository categoriaRepository)
         {
             _produtoRepository = produtoRepository;
+            _categoriaRepository = categoriaRepository;
         }
         public IActionResult Index(int? pagina, string pesquisa)
         {
             var produtos = _produtoRepository.ObterTodosProdutos(pagina, pesquisa);
             return View(produtos);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Select(a => new SelectListItem(a.Nome, a.Id.ToString()));
+            return View();
         }
     }
 }
