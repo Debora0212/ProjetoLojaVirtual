eff82658cf917bd47a6a38b199a4fae06528219c
Debora0212 <debora_s_brito@hotmail.com>
2019-11-28 17:56:49 -0300

    Menu-ViewComponent -Part 1, Part 2, ViewComponent - Menu Multilevel Bootstrap - Part 3.

---
+++ ProjetoLojaVirtual/Libraries/Component/MenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoLojaVirtual.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLojaVirtual.Libraries.Component
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private ICategoriaRepository _categoriaRepository;
+        public MenuViewComponent(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        //Logica do componente, segue uma numeclatura padrao (public async Task<IViewComponentResult>InvokeAsync)
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           var ListaCategoria = _categoriaRepository.ObterTodasCategorias().ToList();
+            return View(ListaCategoria);
+        }
+    }
+}
